fb1e9d336becc76546f9fc01aa14cf38c832ebc7
MartinSHolmes <32949011+MartinSHolmes@users.noreply.github.com>
2019-01-31 19:37:12 -0800

    first commit
    AIdan helped set up Martin's github

---
+++ MartinHolmesCSA/src/unit00/AsciiArt.java
@@ -0,0 +1,35 @@
+package unit00;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Martin Holmes
+//Date - 1/31/19
+//Class - 3
+//Lab  - 00
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Martin Holmes \n\n" );
+		System.out.println("BUNNY" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("        __       __   " );
+		System.out.println("       |  |     |  |  " );
+		System.out.println("       |()|     |()|  " );
+		System.out.println("       |()|     |()|  " );
+		System.out.println("       |()|_____|()|  " );
+		System.out.println("       |           |  " );
+		System.out.println("       | (-)   (-) |  " );
+		System.out.println("       |           |  " );
+		System.out.println("       |   (_|_)   |  " );
+		System.out.println("       |___________|  " );
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
---
+++ MartinHolmesCSA/src/unit00/AsciiBox.java
@@ -0,0 +1,22 @@
+package unit00;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("name \t  date \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("------------------------- " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("0000000000000000000000000 " );
+		System.out.println("+++++++++++++++++++++++++ \r\n------------------------- \r\nAAAAAAAAAAAAAAAAAAAAAAAAA \r\n0000000000000000000000000 " );
+
+	}
+}

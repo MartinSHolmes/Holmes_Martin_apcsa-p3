eae1d1d6ba3e918bdcf6e75ff8e6ec1e364d45fd
MartinSHolmes <32949011+MartinSHolmes@users.noreply.github.com>
2019-02-01 13:20:56 -0800

    Unit02 update
    Unit02 added, Unit03 uploaded

---
+++ MartinHolmesCSA/src/unit02/Trap.java
@@ -0,0 +1,14 @@
+package unit02;
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return (double)(.5*(base1+base2)*height);
+	}
+}
